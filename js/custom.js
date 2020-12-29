6a7648f487a0f2b2e54bfba152accab0e7c86d72
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2020-12-29 21:07:21 +0200

    create section slick slider

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,14 @@
-$('.friends_slider_item').slick({
-    infinite: true,
+$(document).ready(function(){
+  $('.slider').slick({
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='./images/long-left-arrow.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='./images/long-right-arrow.png'>",
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    speed: 1000,
+    easing: 'ease',
+    autoplay: true,
+    autoplaySpeed: 1000,
+    centerMode: true,
   });
+});
+
